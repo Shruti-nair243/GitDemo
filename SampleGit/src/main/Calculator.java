54b3028b75f6ab4dc3e181d8546dacb6cdf4bbed
Shruti <Shruti@DESKTOP-SIKHSGP>
2021-03-23 10:54:53 +0530

    added mmastre2

--- SampleGit/src/main/Calculator.java
+++ SampleGit/src/main/Calculator.java
@@ -41,5 +41,8 @@ public class Calculator {
 	public static void masterfun1(int num1){
 	      System.out.println(num1);
 	  }
+	public static void masterfun2(int num1){
+	      System.out.println(num1);
+	  }
 
 }
